cf0849f95164e002f28560ec39da5ff30a0fda7b
curiouscat90 <vperezmorano@gmail.com>
2023-12-28 15:15:12 +0100

    Añadir aplicación de tiempo en React

---
+++ weather-app/weather.js
@@ -0,0 +1,43 @@
+// src/components/Weather.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+  const [apiKey] = useState('tu-clave-de-api'); // Reemplaza con tu propia clave de API
+
+  const getWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={getWeatherData}>Get Weather</button>
+
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp} K</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
